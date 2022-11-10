137845e7abc25ab323f40602b1d81246274bb955
Antoniolli Laura <laura.antoniolli@outlook.fr>
2022-11-11 00:20:07 +0100

    qualifications ended

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -44,3 +44,22 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills);
 });
+
+/*================= Qualifications =====================*/ 
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active');
+        })
+        target.classList.add('qualification__active');
+        tab.forEach(tab=>{
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualificiation__active');
+    })
+})
