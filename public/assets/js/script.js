5da0716fb6fbe3df111e510f095a1a05fb232d60
Antoniolli Laura <laura.antoniolli@outlook.fr>
2022-11-11 11:22:34 +0100

    services ended

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -63,3 +63,26 @@ tabs.forEach(tab =>{
         tab.classList.add('qualificiation__active');
     })
 })
+
+/*================= MODAL =====================*/ 
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>{
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) =>{
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
